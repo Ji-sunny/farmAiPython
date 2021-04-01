7b167ab39f4de690bb7c61c80c2c6c84eb70f5c3
KYH9216 <andyxoxo@naver.com>
2021-04-01 21:06:07 +0900

    모듈 두개 완성

--- modelingmodule/logisticregression.py
+++ modelingmodule/logisticregression.py
@@ -1,9 +1,9 @@
-
+import pandas as pd
 from dbmodule import dbModule
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 import numpy as np
-
+from sklearn.model_selection import train_test_split
 oracle_db = dbModule.Database()
 
 
@@ -16,16 +16,16 @@ def logesticregression(table_name, cols_X, col_y, pred_cols_X):
     train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3)
 
     # 모델 학습 
-    model = LogisticRegression().fit(X, y)
-    xx = np.linspace(-3, 3, X.shape[1]*X.shape[0])
-    xx = xx.reshape(shape[0], shape[1])
+    model = LogisticRegression(max_iter=5000).fit(train_X,train_y)
+    xx = np.linspace(-1, 1, train_X.shape[1]*train_X.shape[0])
+    xx = xx.reshape(train_X.shape[0], train_X.shape[1])
     XX = xx[:, np.newaxis]
-    XX = XX.reshape(shape[0], shape[1])
-    prob = model.predict_proba(XX)[:,:shape[1]]
-
-    # 시각화
+    XX = XX.reshape(train_X.shape[0], train_X.shape[1])
+    prob = model.predict_proba(XX)[:,:train_X.shape[1]]
+    
+    score = model.score(test_X, test_y)
+    
+    #예측된확률
     X_test =[pred_cols_X]
-    plt.subplot(211)
-    plt.bar(model.classes_, model.predict_proba(X_test)[0])
-    plt.title("temp:{}, humid:{}, ec:{}, ph:{}".format(X_test[0][0],X_test[0][1],X_test[0][2],X_test[0][3]))
-
+    predict = model.classes_, model.predict_proba(X_test)[0]
+    return  predict, score
