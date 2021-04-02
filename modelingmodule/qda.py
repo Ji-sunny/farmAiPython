9a8c4224afe84ebf3bf954c17ea80ef4e0c3a35e
SeungtaeKim <rlatmdxo50@gmail.com>
2021-04-02 11:03:31 +0900

    qda 판별

--- modelingmodule/qda.py
+++ modelingmodule/qda.py
@@ -1,1 +1,23 @@
-# qda 판별
+# Linear/Quadratic Discriminant Analysis
+# QDA(이차판별분석)
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+import pandas as pd
+from sklearn.preprocessing import minmax_scale
+
+from dbmodule import dbModule
+
+oracle_db = dbModule.Database()
+
+def qda(table_name, col_X, col_y):
+    data = oracle_db.read_data_all(table_name)
+
+    X = pd.DataFrame(data, columns=col_X)
+    X = data_minmax_scaled = minmax_scale(X)
+    y = pd.DataFrame(data, columns=[col_y])
+    y = data_minmax_scaled = minmax_scale(y)
+
+    model = QuadraticDiscriminantAnalysis()
+    model.fit(X, y)
+    y_pred = model.predict(X)
+    
+    return X, y, model, y_pred
