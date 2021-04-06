0065a05e5bba5446acef1b47d2c04144527edf08
KYH9216 <andyxoxo@naver.com>
2021-04-07 00:58:21 +0900

    회귀분석

--- modelingmodule/__init__.py
+++ modelingmodule/__init__.py
@@ -1,4 +1,12 @@
 __all__ = [
     'correlation',
-    'linear_regression'
+    'linear_regression',
+    'correlation',
+    'decisiontree',
+    'describe',
+    'feature_importance',
+    'logisticregression',
+    'validation_curve',
+    'rfe'
+
 ]
