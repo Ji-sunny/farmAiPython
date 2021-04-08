295a1c146fdc17d4dda2b4bcc415409be2f5db41
SeungtaeKim <rlatmdxo50@gmail.com>
2021-04-08 18:02:15 +0900

    전처리과정 오류 발견하여 수정

--- module/efarmdesigndetail.py
+++ module/efarmdesigndetail.py
@@ -12,36 +12,34 @@ def efarmdesigndetail(data, table_name):
 #    data = pd.read_csv(efarm, encoding='UTF-8',header=0)
 #    data["date"] = folder_names[0].split('_')[1]  # data열 추가
     #
-
+    print("efarmdesigndetail!!!! data 파라미터의 type확인하기")
+    print(type(data))
     data["site_code"] = data.site_code.str.replace("SITE_FARM", '')
     data["site_code"] = pd.to_numeric(data["site_code"])
     # site_code dtype str --> numpy.int64로 수정
 
     #날짜 0과 NaN값 삭제
-    change_ = {0.0 : np.NaN }
+    change_ = {"0" : np.NaN}
+    # 0: np.NaN --> "0": np.NaN으로 수정해줘야 0값이 NaN으로 바뀜
+    # --> DB에서는 모든 값들을 VARCHAR2로 설정하기 때문에 모두 str값으로 들어옴
     data = data.replace({'work_date': change_})
-    data.dropna(subset=['work_date'],inplace=True)
-    category=['object_type']
+    data.dropna(subset=['work_date'], inplace=True)
+    category = ['object_type']
     data = pd.get_dummies(data, columns=category, prefix_sep='', prefix='')
     # column 순서 수정
 
-    data['work_date'] = pd.to_datetime(data['work_date'], unit='s')
-    data['year'] = data['work_date'].dt.year #year
-    data['month'] = data['work_date'].dt.month #month
-    data['day'] = data['work_date'].dt.day #day
-    data['hour'] = data['work_date'].dt.hour #hour
-    data['minute'] = data['work_date'].dt.minute #min
-    data['second'] = data['work_date'].dt.second #sec
+    data['work_date'] = pd.to_datetime(data['work_date'], unit="s")
+    # unit은 's'면 적용이 안되고, "s" 쌍따옴표로 바꿔줘야 함
+
+    data['year'] = data['work_date'].dt.year  # year
+    data['month'] = data['work_date'].dt.month  # month
+    data['day'] = data['work_date'].dt.day  # day
+    data['hour'] = data['work_date'].dt.hour  # hour
+    data['minute'] = data['work_date'].dt.minute  # min
+    data['second'] = data['work_date'].dt.second  # sec
     data.drop(["work_date"], axis=1, inplace=True)
 
-    data.rename(columns={"id" : "efarm_design_detail_id"}, inplace=True)
-    # data.rename(columns={"_area" : "area"}, inplace=True)
-    # data.rename(columns={"_left" : "left"}, inplace=True)
-    # data.rename(columns={"_top" : "top"}, inplace=True)
-    # data.rename(columns={"_width" : "width"}, inplace=True)
-    # data.rename(columns={"_height" : "height"}, inplace=True)
-    # data.rename(columns={"_real_left" : "real_left"}, inplace=True)
-    # data.rename(columns={"_real_top" : "real_top"}, inplace=True)
-    
+    data.rename(columns={"id": "efarm_design_detail_id"}, inplace=True)
     data.fillna(0, inplace=True)
+
     return data
